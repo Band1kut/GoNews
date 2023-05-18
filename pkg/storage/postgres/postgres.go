a0bb7bec0b02b01fb8cd00bef5a57d517b17880b
Band1kut <glukhead@gmail.com>
2023-05-18 16:23:56 +0300

    Добавлен пакет postgres

--- pkg/storage/postgres/postgres.go
+++ pkg/storage/postgres/postgres.go
@@ -1,6 +1,7 @@
 package postgres
 
 import (
+	"GoNews/pkg/storage"
 	"context"
 	"github.com/jackc/pgx/pgxpool"
 )
@@ -11,13 +12,91 @@ type Store struct {
 }
 
 // New Конструктор объекта хранилища.
-func New(dbUri string) (*Store, error) {
-	db, err := pgxpool.New(context.Background(), dbUri)
+func New(pgUri string) (*Store, error) {
+	db, err := pgxpool.New(context.Background(), pgUri)
 	if err != nil {
 		return nil, err
 	}
-	s := Store{
-		db: db,
-	}
+	s := Store{db: db}
 	return &s, nil
+}
+
+func (s *Store) Posts() ([]storage.Post, error) {
+	rows, err := s.db.Query(context.Background(), `
+		SELECT
+			id,
+			author_id,
+			title,
+			content,
+			created_at
+		FROM posts;`,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var posts []storage.Post
+	for rows.Next() {
+		var p storage.Post
+		err = rows.Scan(
+			&p.ID,
+			&p.AuthorID,
+			&p.Title,
+			&p.Content,
+			&p.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, p)
+	}
+
+	return posts, rows.Err()
+}
+
+func (s *Store) AddPost(p storage.Post) error {
+	_, err := s.db.Exec(context.Background(), `
+		INSERT INTO posts (author_id, title, content, created_at)
+		VALUES ($1, $2, $3, $4);
+		`,
+		p.AuthorID,
+		p.Title,
+		p.Content,
+		p.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) UpdatePost(p storage.Post) error {
+	_, err := s.db.Exec(context.Background(), `
+		UPDATE posts
+		SET title=$1, content=$2
+		WHERE id=$3;`,
+		p.Title,
+		p.Content,
+		p.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+func (s *Store) DeletePost(p storage.Post) error {
+	_, err := s.db.Exec(context.Background(), `
+			DELETE FROM posts
+			WHERE id = $1;`,
+		p.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
